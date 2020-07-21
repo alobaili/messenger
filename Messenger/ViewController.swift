c9461fdc6392f022937f36d6ab765fd7a26be7b8
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2020-07-21 21:27:21 +0300

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Abdulaziz AlObaili on 21/07/2020.
+//  Copyright © 2020 Abdulaziz AlObaili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
